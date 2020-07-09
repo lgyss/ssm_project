52310876b9e8a12d9e0acdbc7bbbc74baad54cee
lg123 <2465214486@qq.com>
2020-07-09 11:15:04 +0800

    删除2

--- springMVC_xm_0610/src/com/newer/datang/data/util/StringUtil.java
+++
@@ -1,8 +0,0 @@
-package com.newer.datang.data.util;
-
-public class StringUtil {
-	//Servlet判断取得的字符串是否为空
-	public static boolean isBlank(String s) {
-		return s == null || "".equals(s.trim());
-	}
-}
